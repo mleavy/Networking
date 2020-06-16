10ad0201b4deea0d4925d47ffad78d0510af358d
Mike Leavy <mleavy@gmail.com>
2020-06-16 14:55:08 -0700

    SPM support

--- Package.swift
+++ Package.swift
@@ -23,5 +23,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "Networking"
+    name: "Networking",
+    platforms: [
+        .iOS(.v10)
+    ],
+    products: [
+        .library(name: "Networking", targets: ["Networking-iOS"])
+    ],
+    targets: [
+        .target(name: "Networking-iOS", path: "Sources")
+    ]
 )
